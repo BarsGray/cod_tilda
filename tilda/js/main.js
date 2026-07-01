2f83751062ba3fa2210d9afd629f039b7532418c
Daniil <m2140sr@gmail.com>
2026-07-01 16:52:20 +0300

    init_code_tilda

---
+++ tilda/js/main.js
@@ -0,0 +1,10 @@
+let bunner_btn_prev = document.querySelector('#molecule-1782397520137000012');
+let bunner_btn_next = document.querySelector('#molecule-1782397561317000013');
+
+bunner_btn_prev.addEventListener('click', () => {
+  console.log('bunner_btn_prev');
+});
+
+bunner_btn_prev.addEventListener('click', () => {
+  console.log('bunner_btn_next');
+});
